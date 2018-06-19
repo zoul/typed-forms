40e13289c0fb49169689724dfa191df691a0e2db
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2018-06-19 12:23:13 +0200

    Extend bindings to all cell types

---
+++ Library/Cells/Bindings.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+protocol Bindable: AnyObject {
+
+    associatedtype Model
+
+    var bindings: [(Model) -> Void] { get set }
+}
+
+extension Bindable {
+
+    public func bind<T>(
+        _ viewPath: WritableKeyPath<Self, T>,
+        to modelPath: KeyPath<Model, T>) {
+        bindings.append { [weak self] model in
+            self?[keyPath: viewPath] = model[keyPath: modelPath]
+        }
+    }
+
+    public func bind<T, U>(
+        _ viewPath: WritableKeyPath<Self, T>,
+        to modelPath: KeyPath<Model, U>,
+        through map: @escaping (U) -> T) {
+        bindings.append { [weak self] model in
+            self?[keyPath: viewPath] = map(model[keyPath: modelPath])
+        }
+    }
+}
--- Library/Cells/FormCell.swift
+++ Library/Cells/FormCell.swift
@@ -1,13 +1,13 @@
 import UIKit
 
-public class FormCell<Model>: UITableViewCell {
+public class FormCell<Model>: UITableViewCell, Bindable {
 
     public var shouldHighlight = false
 
     var update: ((inout Model) -> Void) -> Void = { _ in }
     var didSelect = {}
 
-    private var bindings: [(Model) -> Void] = []
+    var bindings: [(Model) -> Void] = []
 
     public init() {
         super.init(style: .default, reuseIdentifier: nil)
@@ -19,25 +19,5 @@ public class FormCell<Model>: UITableViewCell {
 
     func render(_ model: Model) {
         bindings.forEach { $0(model) }
-    }
-}
-
-public extension FormCell {
-
-    public func bind<T>(
-        _ viewPath: WritableKeyPath<FormCell, T>,
-        to modelPath: KeyPath<Model, T>) {
-        bindings.append { [weak self] model in
-            self?[keyPath: viewPath] = model[keyPath: modelPath]
-        }
-    }
-
-    public func bind<T, U>(
-        _ viewPath: WritableKeyPath<FormCell, T>,
-        to modelPath: KeyPath<Model, U>,
-        through map: @escaping (U) -> T) {
-        bindings.append { [weak self] model in
-            self?[keyPath: viewPath] = map(model[keyPath: modelPath])
-        }
     }
 }
